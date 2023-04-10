f76fa3049cb2355d1683a4f4dddc521c65cb9f68
Kiara <kiarargil@gmail.com>
2023-04-09 22:59:11 -0700

    colors

--- js/style.js
+++ js/style.js
@@ -16,30 +16,3 @@ function randomBGColor() {
   for (var i = 0; i < className.length; i ++) {
       className[i].style.backgroundColor = color; }
 }
-
-// function checkMediaQuery() {
-//     // If the inner width of the window is greater then 768px
-//     if (window.innerWidth < 900) {
-//       // Then log this message to the console
-//       console.log('Media Query Matched!')
-//     }
-//   }
-
-function case_resize() {
-    var  row1 = document.getElementsByClassName("row-1");
-    var row2 = document.getElementsByClassName("row-2");
-    if (window.innerWidth < 820) {
-        console.log(row1.className);
-        // row1[0].className += "-mobile";
-        // row2.className += "-mobile";
-        // console.log(row1[0].className);
-    }
-    else {
-        row1.className = "row-1"
-        // row2.className = "row-2"
-    }
-
-    
-}
-
-case_resize();
