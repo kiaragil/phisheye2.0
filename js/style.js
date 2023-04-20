6352c2e589130a39c61b9a46694724c14585e34a
Kiara <kiarargil@gmail.com>
2023-04-20 15:22:14 -0700

    remove modal

--- js/style.js
+++ js/style.js
@@ -33,22 +33,3 @@ function openPage(pageName,elmnt,color) {
     elmnt.style.backgroundColor = color;
   }
 
-// Modal Setup
-var modal = document.getElementById('modal');
-
-var modalClose = document.getElementById('modal-close');
-modalClose.addEventListener('click', function() { 
-  modal.style.display = "none";
-});
-
-// global handler
-document.addEventListener('click', function (e) { 
-  if (e.target.className.indexOf('modal-target') !== -1) {
-      var img = e.target;
-      var modalImg = document.getElementById("modal-content");
-      // var captionText = document.getElementById("modal-caption");
-      modal.style.display = "block";
-      modalImg.src = img.src;
-      // captionText.innerHTML = img.alt;
-   }
-});
